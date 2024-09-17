718393b26a6e878fd3ef641bee2eff7a6f763d48
Julia Grigoreva <julia_panina@mail.ru>
2024-09-17 17:15:44 +0300

    feat: add covers img and audio files

---
+++ audio-player/js/main.js
@@ -0,0 +1,4 @@
+const player = document.querySelector(".player");
+const btnPlay = document.querySelector(".btn-play");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
